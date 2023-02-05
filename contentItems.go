0564e018bfa3c29c87c82e392c2178531bf26d62
bendows <ben@updated.co.za>
2023-02-05 21:42:39 +0200

    ok

--- contentItems.go
+++ contentItems.go
@@ -7,6 +7,8 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
 
 	logger "github.com/bendows/gologger"
@@ -16,6 +18,75 @@ type ContentItem struct {
 	Hash         string
 	Filename     string
 	Contenttypes []string
+}
+
+func CopyToFile(r io.Reader, filename, directory string) (i int, thefilename string) {
+	diskFileName := ""
+	var f *os.File
+	var err error
+	for {
+		f, err = os.OpenFile(directory+"/"+filename, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
+		if err == nil {
+			diskFileName = directory + "/" + filename
+			logger.Loginfo.Printf("[%s] created\n", directory+"/"+filename)
+			break
+		}
+		secondName := ""
+		for i := 0; i < 100; i++ {
+			secondName = filename + "_" + strconv.Itoa(i)
+			f, err = os.OpenFile(directory+"/"+secondName, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
+			if err == nil {
+				filename = secondName
+				diskFileName = directory + "/" + filename
+				break
+			}
+		}
+		break
+	}
+	if len(diskFileName) < 1 {
+		return 0, ""
+	}
+	fileContents, err := ioutil.ReadAll(r)
+	if err != nil {
+		return 0, ""
+	}
+	// filewriter := bufio.NewWriter(f)
+	// _, err = filewriter.Write(fileContents)
+	var n int
+	n, err = f.Write(fileContents)
+	if err != nil {
+		return 0, ""
+	}
+	logger.Loginfo.Printf("[%d] [%s] file created\n", n, diskFileName)
+	return n, diskFileName
+	// s, err := filewriter.Write(r)
+	// if err != nil || s < 1 {
+	// 	err = f.Close()
+	// 	if err != nil {
+	// 		return file, err
+	// 	}
+	// 	return file, err
+	// }
+	// err = filewriter.Flush()
+	// if err != nil {
+	// 	err = f.Close()
+	// 	if err != nil {
+	// 		return file, err
+	// 	}
+	// 	logger.Logerror.Printf("[%s] [%s] [%v]\n", key, name, err)
+	// 	return file, err
+	// }
+	// logger.Loginfo.Printf("[%s] %d bytes saved\n", key, s)
+	// fileInfo, err := f.Stat()
+	// if err != nil {
+	// 	logger.Logerror.Printf("[%s] [%s] [%v]\n", key, name, err)
+	// 	return file, err
+	// }
+	// err = f.Close()
+	// if err != nil {
+	// 	logger.Logerror.Printf("[%s] [%s] [%v]\n", key, name, err)
+	// 	return file, err
+	// }
 }
 
 func GenerateFileTypesAndHash(r io.Reader) ContentItem {
