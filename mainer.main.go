04f99931e40ce3989275dce1d979bb342e9a310f
Ben <993283+bendows@users.noreply.github.com>
2023-02-07 00:11:45 +0200

    Create mainer.main.go

---
+++ mainer.main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+	"hash"
+	"io"
+	"log"
+	"os"
+
+	logger "github.com/bendows/gologger"
+)
+
+//	https://stackoverflow.com/questions/25671305/golang-io-copy-twice-on-the-request-body
+//
+//	this works for either a reader or writer,
+//
+// but if you use both in the same time the hash will be wrong.
+type Hasher struct {
+	io.Writer
+	io.Reader
+	hash.Hash
+	Size uint64
+}
+
+func (h *Hasher) Write(p []byte) (n int, err error) {
+	n, err = h.Writer.Write(p)
+	logger.Loginfo.Printf("Hasher.Write [%d]\n", n)
+	h.Hash.Write(p)
+	h.Size += uint64(n)
+	return
+}
+
+func (h *Hasher) Read(p []byte) (n int, err error) {
+	n, err = h.Reader.Read(p)
+	logger.Loginfo.Printf("Hasher.Read [%d]\n", n)
+	h.Hash.Write(p[:n]) //on error n is gonna be 0 so this is still safe.
+	return
+}
+
+func (h *Hasher) Sum() string {
+	return hex.EncodeToString(h.Hash.Sum(nil))
+}
+
+type UploadHandle struct {
+	Contents  io.Reader
+	Contents2 io.Reader
+}
+
+func (h *UploadHandle) Read() (io.Reader, string, int64, error) {
+	logger.Loginfo.Printf("Read():)\n")
+	var b bytes.Buffer
+	hashedReader := &Hasher{Reader: h.Contents, Hash: sha1.New()}
+	n, err := io.Copy(&b, hashedReader)
+	if err != nil {
+		return nil, "", 0, err
+	}
+	return &b, hashedReader.Sum(), n, nil
+}
+
+// updated version based on @Dustin's comment since I complete
+func (h *UploadHandle) ReadnewTee() (io.Reader, string, int64, error) {
+	logger.Loginfo.Printf("ReadnewTee():)\n")
+	var b bytes.Buffer
+	hash := sha1.New()
+	n, err := io.Copy(&b, io.TeeReader(h.Contents2, hash))
+	if err != nil {
+		return nil, "", 0, err
+	}
+	return &b, hex.EncodeToString(hash.Sum(nil)), n, nil
+}
+
+func main() {
+	logger.LogOn = true
+	h := UploadHandle{}
+	b, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	// convert byte slice to io.Reader
+	h.Contents = bytes.NewReader(b)
+	_, cnt, d, err := h.Read()
+	if err != nil {
+		log.Fatal()
+	}
+	fmt.Printf("\nhash[%s]\nread-count[%d]\nerr[%s]\n", cnt, d, err)
+	h.Contents2 = bytes.NewReader(b)
+	// updated version based on @Dustin's comment since I complete
+	_, cnt, d, err = h.ReadnewTee()
+	if err != nil {
+		log.Fatal()
+	}
+	fmt.Printf("\nhash[%s]\nread-count[%d]\nerr[%s]\n", cnt, d, err)
+}
+// https://golang.cafe/blog/golang-convert-byte-slice-to-io-reader.html
